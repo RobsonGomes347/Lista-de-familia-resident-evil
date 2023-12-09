037eab0d1b767198e04de77aace209240fdc5ca8
RobsonGomes347 <128649726+RobsonGomes347@users.noreply.github.com>
2023-12-08 22:52:49 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Personagens from "./Personagens";
+
+function App() {
+
+  return (
+    <div>
+
+      <h1>App Familia</h1>
+
+      <Personagens familia="Redfield" />
+
+    </div>
+  );
+}
+
+export default App;
